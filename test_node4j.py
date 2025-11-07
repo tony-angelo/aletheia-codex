85c043908c2623becd68e0ee4bbae70dff780fb1
Tony Angelo <tony@aletheiacodex.com>
2025-11-07 08:50:03 -0700

    Remove sensitive and temporary files from version control

--- test_node4j.py
+++
@@ -1,9 +0,0 @@
-from neo4j import GraphDatabase
-
-# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-URI = "neo4j+s://ac286c9e.databases.neo4j.io"
-AUTH = ("neo4j", "LrVUYKHm7Uu8KWTYlDNnDnWYALD8v9KzdTzPl11WB6E")
-
-with GraphDatabase.driver(URI, auth=AUTH) as driver:
-    driver.verify_connectivity()
-    print("Connection established.")
